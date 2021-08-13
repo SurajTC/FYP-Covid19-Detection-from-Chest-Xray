46bad2f4d50888503ef7c826f8751c53eda2a735
surz <surzavon@gmail.com>
2021-08-13 10:40:33 +0530

    webapp-static updated

--- webapp/components/codegen.py
+++
@@ -1,8 +0,0 @@
-import uuid
-
-def random_string(string_length=4):
-    random = str(uuid.uuid4())
-    random = random.upper()
-    random = random.replace("-","")
-
-    return random[0:string_length]
